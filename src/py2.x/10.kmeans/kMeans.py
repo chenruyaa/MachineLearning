c3ad22bfe54990ee920c090d7c7b0bc32bd84e26
chenruyaa <37565371+chenruyaa@users.noreply.github.com>
2018-04-16 17:37:45 +0800

    Update kMeans.py

--- src/py2.x/10.kmeans/kMeans.py
+++ src/py2.x/10.kmeans/kMeans.py
@@ -53,21 +53,17 @@ def kMeans(dataMat, k, distMeas=distEclud, createCent=randCent):
             minDist = inf
             minIndex = -1
             for j in range(k):
-                distJI = distMeas(centroids[j, :],
-                                  dataMat[i, :])  # 计算数据点到质心的距离
+                distJI = distMeas(centroids[j, :],dataMat[i, :])  # 计算数据点到质心的距离
                 if distJI < minDist:  # 如果距离比 minDist（最小距离）还小，更新 minDist（最小距离）和最小质心的 index（索引）
                     minDist = distJI
                     minIndex = j
             if clusterAssment[i, 0] != minIndex:  # 簇分配结果改变
                 clusterChanged = True  # 簇改变
-                clusterAssment[
-                    i, :] = minIndex, minDist**2  # 更新簇分配结果为最小质心的 index（索引），minDist（最小距离）的平方
+                clusterAssment[i, :] = minIndex, minDist**2  # 更新簇分配结果为最小质心的 index（索引），minDist（最小距离）的平方
         print centroids
         for cent in range(k):  # 更新质心
-            ptsInClust = dataMat[nonzero(
-                clusterAssment[:, 0].A == cent)[0]]  # 获取该簇中的所有点
-            centroids[cent, :] = mean(
-                ptsInClust, axis=0)  # 将质心修改为簇中所有点的平均值，mean 就是求平均值的
+            ptsInClust = dataMat[nonzero(clusterAssment[:, 0].A == cent)[0]]  # 获取该簇中的所有点
+            centroids[cent, :] = mean(ptsInClust, axis=0)  # 将质心修改为簇中所有点的平均值，mean 就是求平均值的
     return centroids, clusterAssment
 
 
